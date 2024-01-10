df5206a8ffc520e534741df27e8ed39f0f94d617
marcebenitez2 <marcebenitez0607@gmail.com>
2024-01-10 20:02:25 -0300

    first commit

---
+++ test.ts
@@ -0,0 +1,56 @@
+// Inferencia: Saben que tipo son sin tener que declararlo.
+
+const a = 2
+const b = 3
+const c = a + b // Ya infiere que C tambien sera un entero
+
+
+// Si tengo una cadena de texto, ya sabe que metodos podes hacer
+
+const cadenadetexto = "Marcelo"
+
+// cadenadetexto. // Ya sabe que metodos tiene
+
+// ❌ cadenadetexto = 2
+
+
+// Funciones
+// Los parametros tambien tienen inferencia de tipos pero se puede especificar
+function saludar(name:string){
+    console.log(`Hola ${name}`)
+}
+
+// Para tipar objetos dentro de una funcion es distinto
+
+function saludar2({name, age}: {name:string, age:number}){
+    console.log(`Hola ${name} tienes ${age} años`)
+}
+
+// TypeScript tiene interferencia en lo que retorna una funcion
+function saludar3(name:string){
+    return `Hola ${name}` //Sabe que aca retorna un string
+}
+
+// En caso de querer especificar el tipo de retorno
+function saludar4(name:string):string{
+    return `Hola ${name}` //Sabe que aca retorna un string
+}
+
+
+// En caso de querer pasarle una funcion como parametro
+//Hay que tener en cuenta que retorna la funcion. En caso de no retornar nada se pone que retorna void, en caso de retornar un string se pone que retorna string
+const sayHiFromFunction = (fn:(name:string)=>void) =>{    
+    fn('Marcelo')
+}
+
+const sayHi = (name:string) => {
+    console.log(`Hola ${name}`)
+}
+
+sayHiFromFunction(sayHi)
+
+// Una funcion tambien puede nunca devolver nada
+
+function throwError(message:string):never{
+    throw new Error(message)
+}
