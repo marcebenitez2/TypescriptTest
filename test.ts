896aa2760390a81b22cf102ecc32d15f937d5ec7
marcebenitez2 <marcebenitez0607@gmail.com>
2024-01-14 19:10:53 -0300

    Update Hero type and createHero function

--- test.ts
+++ test.ts
@@ -86,24 +86,37 @@ avengers.forEach((avengers) => {
 // hero.power = 'Dinero' ❌
 
 type Hero = {
-    name: string,
-    age: number,
-}
+  readonly id?: number; // El signo de pregunta hace que sea opcional y el readonly hace que no se pueda modificar
+  name: string;
+  age: number;
+  isActive?: boolean; // El signo de pregunta hace que sea opcional
+};
+
+// Type Alias
+const hero: Hero = {
+  name: "Ironman",
+  age: 45,
+};
 
 // Se puede hacer que la funcion devuelva un type un objeto usando el type alias como molde
-function createHero(name: string, age: number): Hero {
+function createHero(hero: Hero): Hero {
+  // Tambien se le puede pasar como parametro el type alias
+  const { name, age } = hero;
   return {
     name,
     age,
+    isActive: true,
   };
 }
 
-const thor = createHero("Thor", 45);
+const thor = createHero({ name: "Thor", age: 45 });
+
+// TYPESCRIPT SE EJECUTA SOLO EN DESARROLLO, EN PRODUCCION SE EJECUTA JAVASCRIPT. Por ejemplo si pusiera el readoly , nada mas seria en desarrollo. para que sea en produccion se usa Object.freeze()
 
 
-// Type Alias 
+// Template union types 
 
-const hero: Hero = {
-    name: 'Ironman',
-    age: 45
-}
+// Nos sirve para mantener un conjunto de valores que pueden ser usados en distintos lugares
+type HexadecimalColor = `#${string}`
+// const color : HexadecimalColor = "0033ff" // ❌ No entra en el type porque le falta el # al principio
+const color2 = "#00033f"
